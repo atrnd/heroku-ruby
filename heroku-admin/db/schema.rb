14f9e56910354d9a8108506f3beb2b595429aa04
at-tientvq <tien.tran@asiantech.vn>
2017-11-13 15:04:43 +0700

    reset schema

--- heroku-admin/db/schema.rb
+++ heroku-admin/db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171113033052) do
+ActiveRecord::Schema.define(version: 20171113040350) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "controllers", force: :cascade do |t|
+    t.string "shop"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "shops", force: :cascade do |t|
     t.string "name"
